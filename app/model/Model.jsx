2615077598d51fab0c3547b4a9fa0d354f7535b9
Nick Melms <nick@craftedagency.com>
2023-11-30 13:28:27 -0500

    spin and shadow

--- app/model/Model.jsx
+++ app/model/Model.jsx
@@ -1,63 +1,103 @@
-"use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function Model() {
+  const modelRef = useRef();
+
   useEffect(() => {
     // Scene setup
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       window.innerWidth / window.innerHeight,
-      0.1,
+      0.01,
       1000
     );
-    const renderer = new THREE.WebGLRenderer();
+    camera.position.set(2, 1, 2);
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
     document.body.appendChild(renderer.domElement);
 
-    // Add your model loading and other Three.js logic here
-
-    // Render loop
-    const animate = () => {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-    };
-    animate();
-
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 0, 0); // Set the position of the target the camera looks at
-    controls.update();
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
 
-    // Handle window resize
-    window.addEventListener("resize", () => {
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+    // Lighting
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(5, 10, 7.5);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize.width = 1024;
+    dirLight.shadow.mapSize.height = 1024;
+    dirLight.shadow.camera.near = 0.5;
+    dirLight.shadow.camera.far = 500;
+    scene.add(dirLight);
+
+    // Ground plane to receive shadows
+    const planeGeometry = new THREE.PlaneGeometry(500, 500);
+    const planeMaterial = new THREE.MeshStandardMaterial({
+      color: 0xaaaaaa, // Change this color to your preference
+      roughness: 0.4, // Adjust for surface roughness
+      metalness: 0.2, // Adjust for metalness effect
     });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -Math.PI / 2;
+    plane.position.y = -1;
+    plane.receiveShadow = true;
+    scene.add(plane);
 
+    // Load MTL and OBJ files
     const mtlLoader = new MTLLoader();
     mtlLoader.load("/Cubone/materials.mtl", (materials) => {
       materials.preload();
-
-      // Then, load the model
       const objLoader = new OBJLoader();
-      objLoader.setMaterials(materials); // Set the materials to be used by the OBJLoader
+      objLoader.setMaterials(materials);
       objLoader.load("/Cubone/model.obj", (object) => {
-        // Add the object to your scene
+        modelRef.current = object;
+        object.traverse(function (child) {
+          if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
+        });
         scene.add(object);
       });
     });
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1.5); // Bright white light
-    scene.add(ambientLight);
+    // Handle window resize
+    const onWindowResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
 
-    camera.position.set(2, 2, 0);
-    camera.lookAt(scene.position);
-    const gridHelper = new THREE.GridHelper(10, 10);
+    window.addEventListener("resize", onWindowResize, false);
+
+    // Animation loop
+    const animate = () => {
+      requestAnimationFrame(animate);
+      if (modelRef.current) {
+        modelRef.current.rotation.y += 0.01;
+      }
+      controls.update();
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return () => {
+      document.body.removeChild(renderer.domElement);
+      window.removeEventListener("resize", onWindowResize);
+      controls.dispose();
+    };
   }, []);
 
   return null;
